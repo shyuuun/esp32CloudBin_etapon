64341490647af03aa293954765d7dd2c9be1cf56
shyuuun <m4rshm4ll0w14@gmail.com>
2023-04-29 06:15:28 +0800

    Added sim800l

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
+#define TINY_GSM_MODEM_SIM800
+#include <TinyGsmClient.h>
+#include <HTTPClient.h>
 
 
 
@@ -10,6 +13,11 @@ void servoRight();
 void servoLeft();
 void detectObject();
 void detectMetalObject();
+void RestartGSMModem();
+String GSMRegistrationStatus(RegStatus state);
+String SIMStatus(SimStatus state);
+String GSMSignalLevel(int level);
+void Init_GSM_SIM800();
 
 
 // metal sensor
@@ -34,12 +42,44 @@ void detectMetalObject();
 #define TRIG_RIGHT 18
 #define ECHO_RIGHT 19
 
+// RX/TX Pins for sim 800;
+
+#define RXD2 27
+#define TXD2 14
+
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // 0x3F 
-Servo servoCheck, servoEject, servoBin2;
+Servo servoCheck, servoEject;
+
 
 NewPing sonarCheck(TRIG_CHECK, ECHO_CHECK);
 NewPing sonarLeft(TRIG_LEFT, ECHO_LEFT);
 NewPing sonarRight(TRIG_RIGHT, ECHO_RIGHT);
+
+
+TinyGsm modemGSM(Serial2);
+TinyGsmClient clientGSM(modemGSM);
+
+// Place your APN here
+#define APN_NAME "internet.globe.com.ph"
+#define APN_USER ""
+#define APN_PSWD ""
+
+// auto baud range
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 38400
+
+#define Max_Modem_Reboots 5
+
+// set your cad pin (optional)
+#define SIM_PIN ""
+
+// our server
+String server = "etaponcloud.azurewebsites.net";
+const int port = 80;
+
+int Modem_Reboots_Counter = 0;
+
+HTTPClient *http;
 
 int metal_value = 0;
 int numLeft , numRight, numAll;
@@ -63,7 +103,8 @@ void setup() {
     lcd.setCursor(5,0);   //Set cursor to character 2 on line 0
     lcd.print("E-Tapon");
     delay(5000);
-    lcd.noDisplay();
+
+    Init_GSM_SIM800();
     
 }
 
@@ -203,6 +244,118 @@ void servoRight(){
     delay(1500);
 }
 
+void Init_GSM_SIM800()
+{
+
+    Serial.println("Initialize GSM modem...");
+    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+    Serial.println("Serial GSM Txd is on GPIO" + String(TXD2));
+    Serial.println("Serial GSM Rxd is on GPIO" + String(RXD2));
+
+    delay(2000);
+
+    String info = modemGSM.getModemInfo();
+
+    Serial.println(info);
+
+    if (!modemGSM.restart())
+    {
+        RestartGSMModem();
+    }
+    else
+    {
+        Serial.println("Modem restart OK");
+    }
 
 
+    if (!modemGSM.waitForNetwork())
+    {
+        Serial.println("Failed to connect to network");
+        RestartGSMModem();
+    }
+    else
+    {
+        RegStatus registration = modemGSM.getRegistrationStatus();
+        Serial.println("Registration: [" + GSMRegistrationStatus(registration) + "]");
+        Serial.println("Modem network OK");
+    }
 
+    Serial.println(modemGSM.gprsConnect(APN_NAME, APN_USER, APN_PSWD) ? "GPRS Connect OK" : "GPRS Connection failed");
+
+    bool stateGPRS = modemGSM.isGprsConnected();
+    if (!stateGPRS)
+    {
+        RestartGSMModem();
+    }
+
+    String state = stateGPRS ? "connected" : "not connected";
+    Serial.println("GPRS status: " + state);
+    Serial.println("CCID: " + modemGSM.getSimCCID());
+    Serial.println("IMEI: " + modemGSM.getIMEI());
+    Serial.println("Operator: " + modemGSM.getOperator());
+
+    IPAddress local = modemGSM.localIP();
+    Serial.println("Local IP: " + local.toString());
+
+    int csq = modemGSM.getSignalQuality();
+    if (csq == 0)
+    {
+        Serial.println("Signal quality is 0. Restart modem.");
+        RestartGSMModem();
+    }
+    Serial.println("Signal quality: " + GSMSignalLevel(csq) + " [" + String(csq) + "]");
+}
+
+void RestartGSMModem()
+{
+    Serial.println("Restarting GSM...");
+    if (!modemGSM.restart()) // orignally restart()
+    {
+        Serial.println("\tFailed. :-(\r\n");
+        // ESP.restart();
+    }
+    if (Modem_Reboots_Counter < Max_Modem_Reboots)
+    {
+        Init_GSM_SIM800();
+    }
+    Modem_Reboots_Counter++;
+}
+
+String GSMSignalLevel(int level)
+{
+    switch (level)
+    {
+    case 0:
+        return "-115 dBm or less";
+    case 1:
+        return "-111 dBm";
+    case 31:
+        return "-52 dBm or greater";
+    case 99:
+        return "not known or not detectable";
+    default:
+        if (level > 1 && level < 31)
+            return "-110... -54 dBm";
+    }
+    return "Unknown";
+}
+
+String GSMRegistrationStatus(RegStatus state)
+{
+    switch (state)
+    {
+    case REG_UNREGISTERED:
+        return "Not registered, MT is not currently searching a new operator to register to";
+    case REG_SEARCHING:
+        return "Not registered, but MT is currently searching a new operator to register to";
+    case REG_DENIED:
+        return "Registration denied";
+    case REG_OK_HOME:
+        return "Registered, home network";
+    case REG_OK_ROAMING:
+        return "Registered, roaming";
+    case REG_UNKNOWN:
+        return "Unknown";
+    }
+    return "Unknown";
+}
