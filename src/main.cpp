fd955f20570471abe1b2336d729de36a21e465f1
shyuuun <m4rshm4ll0w14@gmail.com>
2023-06-08 14:34:01 +0800

    currently working code

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 
 
 
-void servoRight();
-void servoLeft();
+void openBin1();
+void openBin2();
 void detectObject();
 void detectMetalObject();
 void RestartGSMModem();
@@ -25,8 +25,10 @@ void Init_GSM_SIM800();
 
 // Check is the servo motor that goes in left and right position of the bin while eject is makes the object falls to the bin 
 
-#define SERVO_CHECK  16
-#define SERVO_EJECT 4
+#define SERVO_BIN1  16
+#define SERVO_BIN2 4
+#define SERVO_SORT 
+#define SERVO_DROP
 
 //#define BUZZER_PIN 13
 
@@ -48,7 +50,7 @@ void Init_GSM_SIM800();
 #define TXD2 14
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // 0x3F 
-Servo servoCheck, servoEject;
+Servo servoBin1, servoBin2, servoSort, servoDrop;
 
 
 NewPing sonarCheck(TRIG_CHECK, ECHO_CHECK);
@@ -84,16 +86,24 @@ HTTPClient *http;
 int metal_value = 0;
 int numLeft , numRight, numAll;
 
+int servoCheckPos = 0;
+
 void setup() {
     Serial.begin(9600);
     // pinMode(BUZZER_PIN, OUTPUT);
-    pinMode(METAL_PIN, INPUT);
     //pinMode(TRIG_PIN1, OUTPUT);
     //pinMode(ECHO_PIN1, INPUT);
     Serial.println("Initialize Metal Sensor");
-    servoCheck.attach(SERVO_CHECK);
-    servoEject.attach(SERVO_EJECT);
-    //servoBin2.attach(SERVO_BIN2_PIN);
+    pinMode(METAL_PIN, INPUT);
+
+    // Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+
+
+    // Initializing LCD screen
     lcd.init();
     Serial.println("Initialize LCD Screen");
     lcd.clear();         
@@ -104,145 +114,45 @@ void setup() {
     lcd.print("E-Tapon");
     delay(5000);
 
-    Init_GSM_SIM800();
+    // Init_GSM_SIM800(); // turn on the sim800l module
     
 }
 
 void loop() {
-    detectObject();
+    // Testing
+
 }
 
+// For Servo motors
 
-// this will ping the two ultrasonic sensors in two bins
-boolean hasDrop(){
-    if(sonarLeft.ping_cm() <= 10 || sonarRight.ping_cm() <= 10){
-        Serial.println("Object Fall Detected");
-        numAll++;
-        Serial.print("Number of objects placed in bin");
-        Serial.println(numAll);
-        return true;
-    } else 
-        Serial.println("Object Fall Not Detected");
-        return false;
-}
+void openBin1(){
+    // Set period of hertz to the servo motor 
+	servoBin1.setPeriodHertz(50);    // standard 50 hz servo
+	servoBin1.attach(SERVO_BIN1); // attaches the servo on pin 18 to the servo object
 
-void detectObject(){
+    Serial.print("Bin 1 opened: ");
+    servoBin1.write(180);
     delay(1500);
-    if (sonarCheck.ping_cm() < 5){
-        lcd.display();
-        lcd.setCursor(0,0);   //Set cursor to character 2 on line 0
-        lcd.print("Object Detected");
-        delay(1500);
-        Serial.println("Object in cylinder detected");
-
-        detectMetalObject();    
-    }else {
-
-        Serial.println("Object in cylinder not detected");
-    
-    }
-}
-
-void detectMetalObject(){
-    metal_value = digitalRead(METAL_PIN);
-    if(metal_value == 0){
-        lcd.setCursor(0,0); 
-        lcd.print("Metal Detected!");
-        Serial.println("Metal Detected");
-
-        // tone(BUZZER_PIN, 5000);
-        /*
-        Testing ko lang ito need pa i-program yung servo motor
-        
-        if(!hasDrop()){
-            Serial.println("Waiting for object to be dropped");
-        } else {
-            numLeft++;
-            Serial.print("Number of objects placed in left bin");
-            Serial.println(numLeft);
-        }
-        */
-        servoLeft();
-
-    }else {
-        lcd.setCursor(0,0); 
-        lcd.print("Non-Metal Detected!");
-        Serial.println("Metal not detected");   
-
-
-        /*
-        if(!hasDrop()){
-            Serial.println("Waiting for object to be dropped");
-        } else {
-            numRight++;
-            Serial.print("Number of objects placed in left bin");
-            Serial.println(numRight);
-        }
-        */
-        servoRight();
-    }
-}
-
-void servoLeft(){
-
-    // go to the left bin
-    servoCheck.writeMicroseconds(1000);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
-    servoCheck.writeMicroseconds(1000);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
-
-    delay(1500);
-    servoEject.write(180);
-    delay(1500);
-
-    // back to normal 
-
-    servoCheck.writeMicroseconds(2000);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
-    servoCheck.writeMicroseconds(2000);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-
-
-    delay(1500);
-    servoEject.write(0);
+    servoBin1.write(0);
     delay(1500);
 }
 
-void servoRight(){
-    // go to right bin
-    servoCheck.writeMicroseconds(2000);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
-    servoCheck.writeMicroseconds(2000);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
+void openBin2(){
+    // Set period of hertz to the servo motor 
+	servoBin2.setPeriodHertz(50);    // standard 50 hz servo
+	servoBin2.attach(SERVO_BIN2); // attaches the servo on pin 18 to the servo object
 
+    Serial.print("Bin 2 opened: ");
+    servoBin2.write(180);
     delay(1500);
-    servoEject.write(180);
-    delay(1500);
-
-    servoCheck.writeMicroseconds(0);
-    delay(150);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
-    servoCheck.writeMicroseconds(0);
-    delay(175);
-    servoCheck.writeMicroseconds(1500);
-    delay(150);
-
-    delay(1500);
-    servoEject.write(0);
+    servoBin2.write(0);
     delay(1500);
 }
+
+
+
+
+// For SIM800L Module 
 
 void Init_GSM_SIM800()
 {
@@ -252,7 +162,6 @@ void Init_GSM_SIM800()
     Serial.println("Serial GSM Txd is on GPIO" + String(TXD2));
     Serial.println("Serial GSM Rxd is on GPIO" + String(RXD2));
 
-    delay(2000);
 
     String info = modemGSM.getModemInfo();
 
